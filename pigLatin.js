f888950daf630a4f78a032657cb4b617eb969033
Summer <summermichiko@gmail.com>
2014-07-23 20:45:42 -0400

    passed all tests

--- pigLatin.js
+++ pigLatin.js
@@ -1,48 +1,33 @@
-var translate = function(word) {
-	var pigla = "ay";
-	var vowels = ["a", "e", "i", "o", "u"]; //use?
-	var firstLetter = word.slice(0,1); //remove
-	var firstTwoLetters = word.slice(0,2); //remove
-	var firstThreeLetters = word.slice(0,3); //remove
-	var s = "";
-	var ending;
-
-	if (firstLetter == "a" || firstLetter == "e" || firstLetter == "i" || firstLetter == "o" || firstLetter == "u"){
-		s = word + pigla;
-		return s; //1
-	}
-
-	if (firstLetter != "a" || firstLetter != "e" || firstLetter != "i" || firstLetter != "o" || firstLetter != "u"){
-		s = word.substring(1) + firstLetter + pigla;
-		console.log(word.substring(1));
-		return s; //2
-	}
-
-	if (firstTwoLetters != "a" || firstTwoLetters != "e" || firstTwoLetters != "i" || firstTwoLetters != "o" || firstTwoLetters != "u") {
-		s = word.substring(2) + firstTwoLetters + pigla;
-		console.log(word.substring(2));
-		return s; //5? should be 3
-	}
-
-	if (firstThreeLetters != "a" || firstThreeLetters != "e" || firstThreeLetters != "i" || firstThreeLetters != "o" || firstThreeLetters != "u") {
-		s = word.substring(3) + firstThreeLetters + pigla;
-		console.log(word.substring(3)); //should be 5
-		return s;
-	}
-	
-};
-
-
-var example = 'example'
-example.search('i')
+function translate(sentence){
+ var vowels = 'aeiou';
+ var index = 0;
+ var finalWordsArray = [];
  
-var hello = 'hello'
-for(var i = 0; i < hello.length; i++){
-  console.log(hello[i])
+ var sentenceArray = sentence.split(" ");
+ console.log(sentenceArray);
+    
+    
+    sentenceArray.forEach(function(word){ //can't use break statement
+        for(var i = 0; i < word.length; i++){
+            
+            if(vowels.search(word[i]) !== -1){
+                if(word[i] != "u" || (word[i] === "u" && word[i - 1] != 'q') ){
+                    index = i ;
+                    break;
+                }
+                
+            }     
+        }
+            
+        finalWordsArray.push(word.substring(index, word.length) + word.substring(0, index) + 'ay');
+                                                       //apple            "" + 'ay" === appleay
+                                                       //erry        + 'ch' + "ay"
+                                                       //iet       + qu   + ietquay
+
+    })
+    
+    return finalWordsArray.join(" ");
+    
 }
-
-
-
-
 
 
