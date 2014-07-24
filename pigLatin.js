709d7dec488a51934ee5d9a1ac4460f2a05791fe
Summer <summermichiko@gmail.com>
2014-07-24 13:40:13 -0400

    final

--- pigLatin.js
+++ pigLatin.js
@@ -6,28 +6,21 @@ function translate(sentence){
  var sentenceArray = sentence.split(" ");
  console.log(sentenceArray);
     
-    
     sentenceArray.forEach(function(word){ //can't use break statement
         for(var i = 0; i < word.length; i++){
-            
             if(vowels.search(word[i]) !== -1){
                 if(word[i] != "u" || (word[i] === "u" && word[i - 1] != 'q') ){
                     index = i ;
                     break;
-                }
-                
+                }       
             }     
-        }
-            
+        }   
         finalWordsArray.push(word.substring(index, word.length) + word.substring(0, index) + 'ay');
                                                        //apple            "" + 'ay" === appleay
                                                        //erry        + 'ch' + "ay"
                                                        //iet       + qu   + ietquay
-
     })
-    
     return finalWordsArray.join(" ");
-    
 }
 
 
