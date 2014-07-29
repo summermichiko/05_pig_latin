61700606a5723907c618872af3c8596a25c26609
Summer <summermichiko@gmail.com>
2014-07-29 19:40:27 -0400

    added review notes

--- pigLatin.js
+++ pigLatin.js
@@ -1,17 +1,20 @@
 function translate(sentence){
- var vowels = 'aeiou';
- var index = 0;
- var finalWordsArray = [];
+    var vowels = "aeiou";
+    var index = 0;
+    var finalWordsArray = [];
  
- var sentenceArray = sentence.split(" ");
- console.log(sentenceArray);
+    var sentenceArray = sentence.split(" "); //changing sentence string into an array
     
-    sentenceArray.forEach(function(word){ //can't use break statement
-        for(var i = 0; i < word.length; i++){
-            if(vowels.search(word[i]) !== -1){
-                if(word[i] != "u" || (word[i] === "u" && word[i - 1] != 'q') ){
-                    index = i ;
-                    break;
+    sentenceArray.forEach(function(word){ //looping over each word
+        console.log(word);
+
+        for(var i = 0; i < word.length; i++){ //looping over each letter in each word
+            console.log(word[i]);
+            if(vowels.search(word[i]) !== -1){ //if word[i] is not in the string vowels, it will return -1, otherwise it will return the index number
+                //i has an index of 2
+                if(word[i] !== "u" || (word[i] === "u" && word[i - 1] !== 'q') ){
+                   index = i ;
+                   break; //stops executing code at this point
                 }       
             }     
         }   
@@ -19,7 +22,7 @@ function translate(sentence){
                                                        //apple            "" + 'ay" === appleay
                                                        //erry        + 'ch' + "ay"
                                                        //iet       + qu   + ietquay
-    })
+    });
     return finalWordsArray.join(" ");
 }
 
